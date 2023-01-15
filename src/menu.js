8194c8b1f6b910c3b0f5a6c1c9845f89fb924fd3
James Olsen <olsenjames1116@gmail.com>
2023-01-15 10:47:21 -0700

    Add images to menu items

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,12 @@
+import fightMilkImage from './images/fightMilk.jpg';
+import riotJuiceImage from './images/riotJuice.jpg';
+import beerImage from './images/beer.jpg';
+import rumHamImage from './images/rumHam.jpg';
+import grilledCharlieImage from './images/grilledCharlie.jpg';
+import milkSteakImage from './images/milkSteak.jpg';
+import macNCheeseImage from './images/macNCheese.jpg';
+import grilledFrankImage from './images/grilledFrank.jpg';
+
 const elementFactory = (selector, id, ...children) => {
     const element = document.createElement(selector);
 
@@ -40,9 +49,9 @@ function clearMainContent() {
 
 function loadDrinks() {
     const drinksHeader = elementFactory('h2', null, 'Drinks');
-    const fightMilk = menuFactory('fightMilk', 'Fight Milk', 'An alcoholic, dairy-based protein drink for bodyguards by bodyguards.');
-    const riotJuice = menuFactory('riotJuice', 'Riot Juice', 'Made to be consumed in public and at a potentially violent sporting event.');
-    const beer = menuFactory('beer', 'Beer', 'The gang\'s drink of choice.');
+    const fightMilk = menuFactory('fightMilk', 'Fight Milk', 'An alcoholic, dairy-based protein drink for bodyguards by bodyguards.', fightMilkImage);
+    const riotJuice = menuFactory('riotJuice', 'Riot Juice', 'Made to be consumed in public and at a potentially violent sporting event.', riotJuiceImage);
+    const beer = menuFactory('beer', 'Beer', 'The gang\'s drink of choice.', beerImage);
 
     const drinks = elementFactory('div', 'drinks', drinksHeader, fightMilk, riotJuice, beer);
     return drinks;
@@ -50,11 +59,11 @@ function loadDrinks() {
 
 function loadFood() {
     const foodHeader = elementFactory('h2', null, 'Food');
-    const rumHam = menuFactory('rumHam', 'Rum Ham', 'Ham, soaked in rum. It\'s loaded with booze.');
-    const grilledCharlie = menuFactory('grilledCharlie', 'Grilled Charlie', 'Peanut butter outside, chocolate inside, butter inside, cheese outside.');
-    const milkSteak = menuFactory('milkSteak', 'Milk Steak', 'Ribeye, boiled in milk and topped with jelly beans.');
-    const macNCheese = menuFactory('macNCheese', 'Mac\'s Famous Mac N Cheese', 'Mac\'s specialty dish. Served with mystery meat.');
-    const grilledFrank = menuFactory('grilledFrank', 'Grilled Frank', 'Sausage, spam and bacon wrapped in a jelly pancake and cooked with a stick of butter.');
+    const rumHam = menuFactory('rumHam', 'Rum Ham', 'Ham, soaked in rum. It\'s loaded with booze.', rumHamImage);
+    const grilledCharlie = menuFactory('grilledCharlie', 'Grilled Charlie', 'Peanut butter outside, chocolate inside, butter inside, cheese outside.', grilledCharlieImage);
+    const milkSteak = menuFactory('milkSteak', 'Milk Steak', 'Ribeye, boiled in milk and topped with jelly beans.', milkSteakImage);
+    const macNCheese = menuFactory('macNCheese', 'Mac\'s Famous Mac N Cheese', 'Mac\'s specialty dish. Served with mystery meat.', macNCheeseImage);
+    const grilledFrank = menuFactory('grilledFrank', 'Grilled Frank', 'Sausage, spam and bacon wrapped in a jelly pancake and cooked with a stick of butter.', grilledFrankImage);
 
     const food = elementFactory('div', 'food', foodHeader, rumHam, grilledCharlie, milkSteak, macNCheese, grilledFrank);
     return food;
